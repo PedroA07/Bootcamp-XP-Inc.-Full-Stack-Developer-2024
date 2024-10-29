10e1d6bffbc03335035090fe45e7f4fc49d07eb4
Pedro Andrade <pedro.andrade.pereira@hotmail.com.br>
2024-10-29 00:46:32 -0300

    correções

--- 5. Operadores Lógicos/Program.cs
+++ 5. Operadores Lógicos/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Operador OR
+
+bool maiorDeIdade = true;
+bool possuiAutorizacao = false;
+
+if (maiorDeIdade || possuiAutorizacao) {
+    Console.WriteLine("Entrada liberada");
+} else {
+    Console.WriteLine("Entrada negada");
+}
+
+// Operador AND
+
+bool possuiPresencaMinima = true;
+double media = 7.5;
+
+if (possuiPresencaMinima && media >= 7) {
+    Console.WriteLine("Aprovado");
+} else {
+    Console.WriteLine("Reprovado");
+}
+
+// Operador NOT
+
+bool choveu = true;
+bool estaTarde = true;
+
+if (!choveu && !estaTarde) { // Para negar uma variável, basta apenas adicionar o sinal de "!" antes dela
+    Console.WriteLine("Vou pedalar");
+} else {
+    Console.WriteLine("Não vou pedalar");
+}
